b677689f93f9b03fdceac8835202ba43352131e3
TMariaA <phoenix_ignea@mail.ru>
2022-03-29 09:58:01 +0000

    transfer piece of page-proofs

--- o-servise/kontakty/index.php
+++ o-servise/kontakty/index.php
@@ -1,16 +1,24 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
-?><? $APPLICATION->IncludeComponent(
-    "bitrix:main.include",
-    ".default",
-    array(
-        "AREA_FILE_SHOW" => "file",
-        "COMPONENT_TEMPLATE" => ".default",
-        "EDIT_TEMPLATE" => "",
-        "PATH" => SITE_TEMPLATE_PATH . "/include/contacts.php"
-    )
-); ?>
+?>
+    <div class="site-section border-bottom">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <? $APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    ".default",
+                    array(
+                        "AREA_FILE_SHOW" => "file",
+                        "COMPONENT_TEMPLATE" => ".default",
+                        "EDIT_TEMPLATE" => "",
+                        "PATH" => SITE_TEMPLATE_PATH . "/include/contacts.php"
+                    )
+                ); ?>
+            </div>
+        </div>
+    </div>
     <div class="site-section border-bottom">
         <div class="container">
             <div class="row align-items-center">
