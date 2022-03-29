deb9edd2e39e6230f90f16a916cc733a6d39a43b
TMariaA <phoenix_ignea@mail.ru>
2022-03-29 09:52:20 +0000

    edit index.php(contacts) and include file from index.php(contacts)(transfer map)

--- local/templates/home/include/contacts.php
+++ local/templates/home/include/contacts.php
@@ -11,22 +11,6 @@
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus in cum odio.</p>
                 <p>Illum repudiandae ratione facere explicabo. Consequatur dolor optio iusto, quos autem voluptate ea? Sunt laudantium fugiat, mollitia voluptate? Modi blanditiis veniam nesciunt architecto odit voluptatum tempore impedit magnam itaque natus!</p>
             </div>
-            <div class="col-md-5 ml-auto"  data-aos="fade-up" data-aos-delay="200">
-            <?$APPLICATION->IncludeComponent(
-                "bitrix:map.yandex.view",
-                "",
-                Array(
-                    "API_KEY" => "",
-                    "CONTROLS" => array("ZOOM","MINIMAP","TYPECONTROL","SCALELINE"),
-                    "INIT_MAP_TYPE" => "MAP",
-                    "MAP_DATA" => "a:3:{s:10:\"yandex_lat\";s:7:\"55.7383\";s:10:\"yandex_lon\";s:7:\"37.5946\";s:12:\"yandex_scale\";i:10;}",
-                    "MAP_HEIGHT" => "500",
-                    "MAP_ID" => "",
-                    "MAP_WIDTH" => "600",
-                    "OPTIONS" => array("ENABLE_SCROLL_ZOOM","ENABLE_DBLCLICK_ZOOM","ENABLE_DRAGGING")
-                )
-            );?>
-            </div>
         </div>
     </div>
 </div>
--- o-servise/kontakty/index.php
+++ o-servise/kontakty/index.php
@@ -1,13 +1,34 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	".default",
-	Array(
-		"AREA_FILE_SHOW" => "file",
-		"COMPONENT_TEMPLATE" => ".default",
-		"EDIT_TEMPLATE" => "",
-		"PATH" => SITE_TEMPLATE_PATH."/include/contacts.php"
-	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><? $APPLICATION->IncludeComponent(
+    "bitrix:main.include",
+    ".default",
+    array(
+        "AREA_FILE_SHOW" => "file",
+        "COMPONENT_TEMPLATE" => ".default",
+        "EDIT_TEMPLATE" => "",
+        "PATH" => SITE_TEMPLATE_PATH . "/include/contacts.php"
+    )
+); ?>
+    <div class="site-section border-bottom">
+        <div class="container">
+            <div class="row align-items-center">
+                <? $APPLICATION->IncludeComponent(
+                    "bitrix:map.yandex.view",
+                    "",
+                    array(
+                        "API_KEY" => "",
+                        "CONTROLS" => array("ZOOM", "MINIMAP", "TYPECONTROL", "SCALELINE"),
+                        "INIT_MAP_TYPE" => "MAP",
+                        "MAP_DATA" => "a:3:{s:10:\"yandex_lat\";s:7:\"55.7383\";s:10:\"yandex_lon\";s:7:\"37.5946\";s:12:\"yandex_scale\";i:10;}",
+                        "MAP_HEIGHT" => "500",
+                        "MAP_ID" => "",
+                        "MAP_WIDTH" => "600",
+                        "OPTIONS" => array("ENABLE_SCROLL_ZOOM", "ENABLE_DBLCLICK_ZOOM", "ENABLE_DRAGGING")
+                    )
+                ); ?>
+            </div>
+        </div>
+    </div>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
